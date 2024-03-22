7dda290cd6fa3638d9778c44a9bfccfbcf3a1b8e
zixunh <zixun@berkeley.edu>
2024-03-21 18:36:05 -0700

    added parking seq

--- run_nuscenes.sh
+++ run_nuscenes.sh
@@ -2,3 +2,4 @@
 python demo.py --imagedir=data/nuscenes008_front --calib=calib/nuscenes.txt --stride=2 --disable_BA
 
 # nuscenes parking w/o BA [03/21/24]
+python demo.py --imagedir=data/nuparking008_front --calib=calib/nuscenes.txt --stride=2 --disable_BA
